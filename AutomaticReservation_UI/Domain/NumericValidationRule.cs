6ecc3aa5a710586cf35c1bbd6603bf69bf0eeb43
Ryosuke UCHIYAMA <uchi63rs@yahoo.co.jp>
2019-04-13 18:29:29 +0900

    電話番号の数字チェックを追加

---
+++ AutomaticReservation_UI/Domain/NumericValidationRule.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace AutomaticReservation_UI.Domain
+{
+    public class NumericValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return ((string)value).Any(c => !char.IsNumber(c))
+                ? new ValidationResult(false, "数字のみ")
+                : ValidationResult.ValidResult;
+        }
+    }
+}
